cf0a1c7fad1f8b9fc2869f8c9c902b286728fb41
niezl <nzlwan@126.com>
2018-01-22 16:13:52 +0800

    图片

---
+++ zg69.cn/Controllers/LCController.cs
@@ -0,0 +1,35 @@
+﻿using _69zg.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zg69.cn.Controllers
+{
+    public class LCController : ILController
+    {
+        
+        public ActionResult Img(string pwd)
+        {
+            if (pwd != "0474")
+                return RedirectToAction("Index");
+            return View();
+        }
+
+        public ActionResult UploadFile()
+        {
+            if (RequestUtil.GetValue("niezl") != "niezl")
+            {
+                return new EmptyResult();
+            }
+            if (Request.Files.Count > 0)
+            {
+                Request.Files[0].SaveAs(Server.MapPath("~/Files/" + Request.Files[0].FileName));
+                return ObjResult(true, "上传成功");
+            }
+            else
+                return View();
+        }
+    }
+}
